d7c14f1e8a99c9dfe0ad1f380ef00c90a96ee4e0
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2022-11-23 17:43:36 -0800

    refactor: reduced redundancy in conversion script

--- scripts.js
+++ scripts.js
@@ -1,31 +1,51 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
-//add a click even to our button
-//store the value of our input into a variable
-// for each div we want to output a <p> with the conversion 
-
 // lay out input element and button element
-const inputEl = document.getElementById("measure");
-const buttonEl = document.getElementById("converter");
+const inputEl = document.querySelector(".measure");
+const buttonEl = document.querySelector(".converter");
 // lay out element for each output
-const lengthEl = document.getElementById("length");
-const volEl = document.getElementById("vol");
-const massEl = document.getElementById("mass");
-const elArr = [lengthEl, volEl, massEl]
-const lengthConv = 3.281
-const volConv = .264
-const massConv = 2.204
+const lengthEl = document.querySelector(".length");
+const volEl = document.querySelector(".vol");
+const massEl = document.querySelector(".mass");
+const elArr = [lengthEl, volEl, massEl];
 
+// grabbing elements that contain output values that need to be updated once conversion happens
+const beginningValues = document.querySelectorAll(".usr-val")
+const firstOutputValues = document.querySelectorAll(".first-conv")
+const secondOutputValues = document.querySelectorAll(".second-conv")
+
+// conversion factors for each measure type
+const lengthConvFactor = 3.281;
+const volConvFactor = .264;
+const massConvFactor = 2.204;
+
+// functionality for our convertor button
 buttonEl.addEventListener("click", function() {
-    //remove all child nodes here
-    const number = Number(inputEl.value)
-    // convert our measurement into meters & feet / liters & gallons etc.
-    // push a string into the textContent of each p
-    lengthEl.textContent = `${number} meters = ${(number * lengthConv).toFixed(3)} feet | ${number} feet = ${(number / lengthConv).toFixed(3)} meters`
-    volEl.textContent = `${number} liters = ${(number * volConv).toFixed(3)} gallons | ${number} gallons = ${(number / volConv).toFixed(3)} liters`
-    massEl.textContent = `${number} kilograms = ${(number * massConv).toFixed(3)} pounds | ${number} pounds = ${(number / massConv).toFixed(3)} kilograms`
+    // convert the string the user inputs into a #
+    const numberToConvert = Number(inputEl.value)
+
+    // update output spans to contain the # we're converting from
+    beginningValues.forEach((value) => {
+        value.textContent = `${numberToConvert}`
+    })
+
+    // update output spans to contain the #'s we're converting to (imperial to metric)
+    firstOutputValues.forEach((value) => {
+        if (value.classList.contains("len-out")) {
+            value.textContent = `${(numberToConvert * lengthConvFactor).toFixed(3)}`
+        } else if (value.classList.contains("vol-out")) {
+            value.textContent = `${(numberToConvert * volConvFactor).toFixed(3)}`
+        } else {
+            value.textContent = `${(numberToConvert * massConvFactor).toFixed(3)}`
+        }
+    })
+
+    // update output spans to contain the #'s we're converting to (metric to imperial)
+    secondOutputValues.forEach((value) => {
+        if (value.classList.contains("len-out")) {
+            value.textContent = `${(numberToConvert / lengthConvFactor).toFixed(3)}`
+        } else if (value.classList.contains("vol-out")) {
+            value.textContent = `${(numberToConvert / volConvFactor).toFixed(3)}`
+        } else {
+            value.textContent = `${(numberToConvert / massConvFactor).toFixed(3)}`
+        }
+    })
 })
