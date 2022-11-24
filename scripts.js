bd175127551f19f148dd12797945dbfce9688d22
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2022-11-23 22:54:14 -0800

    refactor: renamed variables & funcs for clarity

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@ const inputEl = document.querySelector(".measure");
 const buttonEl = document.querySelector(".converter");
 
 // grabbing elements that contain output values that need to be updated once conversion happens
-const beginningValues = document.querySelectorAll(".usr-val")
-const firstOutputValues = document.querySelectorAll(".first-conv")
-const secondOutputValues = document.querySelectorAll(".second-conv")
+const usrVal = document.querySelectorAll(".usr-val")
+const firstConv = document.querySelectorAll(".first-conv")
+const secondConv = document.querySelectorAll(".second-conv")
 
 // functionality for our convertor button
 buttonEl.addEventListener("click", function() {
@@ -13,22 +13,22 @@ buttonEl.addEventListener("click", function() {
     const numberToConvert = Number(inputEl.value)
 
     // update output spans to contain the # we're converting from
-    beginningValues.forEach((value) => {
+    usrVal.forEach((value) => {
         value.textContent = `${numberToConvert}`
     })
 
     // update output spans to contain the #'s we're converting to (imperial to metric)
-    converter(numberToConvert, firstOutputValues, true);
+    outputTextContentConverter(numberToConvert, firstConv, true);
     // update output spans to contain the #'s we're converting to (metric to imperial)
-    converter(numberToConvert, secondOutputValues, false);
+    outputTextContentConverter(numberToConvert, secondConv, false);
 })
 
 // conversion helper function - third parameter determines what conversion factor we use
-function converter(numberToConvert, output, impToMetric) {
+function outputTextContentConverter(numberToConvert, output, isImpToMetric) {
     // determining conversion factor based on if imp -> metric is true 
-    const lengthConvFactor = impToMetric ? 3.281 : 1 / 3.281;
-    const volConvFactor = impToMetric ? .264 : 1 / .264 ;
-    const massConvFactor = impToMetric ? 2.204 : 1 / 2.204;
+    const lengthConvFactor = isImpToMetric ? 3.281 : 1 / 3.281;
+    const volConvFactor = isImpToMetric ? .264 : 1 / .264 ;
+    const massConvFactor = isImpToMetric ? 2.204 : 1 / 2.204;
 
     // converting and updating text values conv factors have been set
     output.forEach((value) => {
